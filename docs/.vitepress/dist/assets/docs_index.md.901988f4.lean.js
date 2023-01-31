0d2949e5a1934c7456d0f50e2203dc5bbb5dfa9f
saracalihan <saracalihan@gmail.com>
2023-01-31 03:27:43 +0300

    .

--- docs/.vitepress/dist/assets/docs_index.md.901988f4.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o as t,a as o}from"./app.a96331c1.js";const p=JSON.parse('{"title":"Detailed Documantation","description":"","frontmatter":{},"headers":[],"relativePath":"docs/index.md","lastUpdated":1675123433000}'),d={name:"docs/index.md"},s=o("",5),n=[s];function c(l,i,r,_,h,m){return t(),a("div",null,n)}const f=e(d,[["render",c]]);export{p as __pageData,f as default};
